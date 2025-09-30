0dfcda267566ea87eeb3ab677dc599915ed3cdea
Justin-git-cy <justinmj2006@gmail.com>
2025-09-30 10:04:58 +0530

    Create Ticket_output.java

---
+++ JAVA(CS2053)/Lab6/Ticket_output.java
@@ -0,0 +1,11 @@
+User 1 booked a ticket.
+Tickets remaining: 4
+User 2 booked a ticket.
+Tickets remaining: 3
+User 3 booked a ticket.
+Tickets remaining: 2
+User 4 booked a ticket.
+Tickets remaining: 1
+User 5 booked a ticket.
+Tickets remaining: 0
+No tickets available for User 6
