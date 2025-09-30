0e41ed304e5965f1d672e5961f035164b5cb7c60
Justin-git-cy <justinmj2006@gmail.com>
2025-09-30 10:03:47 +0530

    Create Table_output.java

---
+++ JAVA(CS2053)/Lab6/Table_output.java
@@ -0,0 +1,10 @@
+Thread 1 prints: 5 x 1 = 5
+Thread 1 prints: 5 x 2 = 10
+Thread 1 prints: 5 x 3 = 15
+Thread 1 prints: 5 x 4 = 20
+Thread 1 prints: 5 x 5 = 25
+Thread 1 prints: 5 x 6 = 30
+Thread 1 prints: 5 x 7 = 35
+Thread 1 prints: 5 x 8 = 40
+Thread 1 prints: 5 x 9 = 45
+Thread 1 prints: 5 x 10 = 50
